348f493c213889112437ce1019a9774d8ab64da4
imranacmo <imran.mir@acmo.co.in>
2022-03-27 18:24:38 +0530

    initial commit

---
+++ app/Http/Controllers/Api/Registration.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Registration extends Controller
+{
+    function try()
+    {
+        echo "hello";
+    }
+}
