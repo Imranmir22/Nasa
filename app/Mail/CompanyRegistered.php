4e9edb78a40f72ed71f3c656a01fab46bd5cbb4c
imranacmo <imran.mir@acmo.in>
2022-10-31 08:56:51 +0530

    mail class for company regsitered

---
+++ app/Mail/CompanyRegistered.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Envelope;
+
+class CompanyRegistered extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+
+    public function envelope()
+    {
+        return new Envelope(
+            new Address('jeffrey@example.com', 'Jeffrey Way'),
+           'Order Shipped',
+        );
+    }
+}
